38447b8c94f034d4879ee5fc96c96c020bd7239f
natalie <nataliemae263@gmail.com>
2022-02-17 16:49:11 -0600

    fingers crossed

---
+++ JavaScript/main.js
@@ -0,0 +1,25 @@
+
+// class card {
+//     constructor(suit, value) {
+//         this.suit = suit;
+//         this.value = value;
+//     }
+// }
+
+function deckBuilder() {
+    const suits = ['spades', 'hearts', 'clubs', 'diamonds'];
+    const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+    const cards = [];
+
+    for(let i = 0; i < suits.length; i++){
+
+}
+// create a loop of some sort to match up the values and suits. They created
+//a for loop to make the cards cycle through the whole deck. I believe
+//I should be doing a do...while loop for the game of War.
+}
+
+return cards();
+
+
+//async await? 
