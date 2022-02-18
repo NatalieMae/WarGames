8fbead55742fdeeeeaca4c241945a85576335226
natalie <nataliemae263@gmail.com>
2022-02-17 22:09:07 -0600

    darn JS I will get you!

--- JavaScript/cards.js
+++ JavaScript/cards.js
@@ -2,40 +2,41 @@ function card(value, name, suit) {
     this.value = value;
     this.name = name;
     this.suit = suit;
-
+}
 
 function deck() {
-        this.names = ['A', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+        this.names = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
         this.suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds'];
         let cards = []; 
     
-    for(let v = 0; v <this.names.length; v++) {
-        for(let s = 0; s <this.suits.length; s++)
-            card.push( new card( n+1, this.name[n], this.suits[s]));
+    for(let n = 0; n <this.names.length; n++) {
+        for(let s = 0; s <this.suits.length; s++){
+            cards.push( new card( n+1, this.names[n], this.suits[s]));
     }
 }
-
 return cards;
+}
+
+
 
 
 
 let myDeck = new deck();
-console.log(myDeck);
+
 
 window.onload = function() {
 
     for(let i=0; i < myDeck.length; i++) {
         div = document.createElement('div');
-        div.className = 'card';
+        div.className = 'cards';
         
         if(myDeck[i].suit === 'Diamonds') {
-            let ascii_char = '';
+            let ascii_char = '&diams;';
         } else {
             let ascii_char = '&' + myDeck[i].suit.toLowerCase() + ';';
         }
-        div.innerHTML = '' + myDeck[i].name + '' + ascii_char + '';
+        div.innerHTML = '.number' + myDeck[i].name + '.suit' + ascii_char + '';
         document.body.appendChild(div);
         }
     }
-}
-console.log();
+
--- JavaScript/main.js
+++ JavaScript/main.js
@@ -5,23 +5,25 @@
 //         this.value = value;
 //     }
 // }
+const suits = ['spades', 'hearts', 'clubs', 'diamonds'];
+const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 
-function deckBuilder() {
-    const suits = ['spades', 'hearts', 'clubs', 'diamonds'];
-    const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+function deckBuilder() 
+{
+    let deck = new Array();
     
-    const cards = [];
-    for(let s = 0; s < suits.length; s++){
-        for(let v = 0; v < values.length; v++) {
-            const value = values(v);
-            const suit = suits(s);
-            cards.push({ value, suit });
-            console.log(deckBuilder());
+for(let s = 0; s < suits.length; s++) 
+    {
+    for(let v = 0; v < values.length; v++) 
+        {
+            let card = {Value: values[x], Suit: suits[i]};
+            deck.push(card);
         }
     }
+        return deck;
 }
 // create a loop of some sort to match up the values and suits. They created
 //a for loop to make the cards cycle through the whole deck. I believe
 //I should be doing a do...while loop for the game of War.
 
-// return cards;
+
